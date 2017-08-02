2442d4b5136e6fe41d7bf69d677d63094474e936
drofsirreb <30606220+drofsirreb@users.noreply.github.com>
2017-08-02 23:10:25 +0100

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,14 @@
+rrange(10);
+
+function rrange(r){
+	for(var z=0;z<r;z++){
+		document.write("<p>");
+	for(var y=0;y<r;y++){
+	for(var x=0;x<r;x++){
+		document.write(x);
+	}
+	document.write("<br>");
+}
+}
+document.write("</p>");
+}
